9c99b7382df05e548150fb503492db848e9842d4
mroy35034 <mroy35034@gmail.com>
2022-05-10 21:21:05 +0600

    firebase added

--- src/index.js
+++ src/index.js
@@ -3,22 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
-      domain="dev-hptycpbi.us.auth0.com"
-      clientId="4sDIlyZlpoVv2zIdFiN5oUvkZ3eP7izN"
-      redirectUri={window.location.origin}
-    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Auth0Provider>
-
   </React.StrictMode>
 );
 
